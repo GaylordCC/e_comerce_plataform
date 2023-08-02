6b9fe3cf8ef010bbbffef3769e6c8eab3c5ad9b4
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-08-02 15:06:42 -0500

    Add profiles and show their products

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,8 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_name: { new: '/' }
   end
 
+  resources :users, only: :show, path: '/user', param: :username
   resources :categories, except: :show
-  # delete '/products/:id', to: 'products#destroy'
-  # patch '/products/:id', to: 'products#update'
-  # post '/products', to: 'products#create'
-  # get '/products/new', to: 'products#new', as: :new_product
-  # get '/products/:id', to: 'products#show', as: :product
-  # get '/products', to: 'products#index'
-  # get '/products/:id/edit', to: 'products#edit', as: :edit_product
   resources :products, path: '/'
 
 end
